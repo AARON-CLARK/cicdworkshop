5ed0202051cad5862066c806b59d29911a77c745
Aaron Clark <aaron.clark@biogen.com>
2022-11-04 12:12:48 -0400

    fix linter cyclical problem

--- R/hello.R
+++ R/hello.R
@@ -22,10 +22,6 @@ hello <- function() {
 #' linter_ex("lint")
 linter_ex <- function(x) {
   if (x == "lint") {
-    if (x == "lint") {
-      if (x == "lint") {
-        message("Whoa!")
-      }
-    }
+    message("Whoa!")
   }
 }
